ea2a4762da69d0d086d8f405fe08c430c6dc3706
jul64 <julie.sehedic@ca-ts.fr>
2016-04-19 16:22:53 +0200

    recup env var

--- connexion.php
+++ connexion.php
@@ -1,9 +1,9 @@
 <?php
-define( 'DB_HOST', 'db' );
-define( 'DB_PORT', '3306' );
-define( 'DB_NAME', 'contact' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', getenv("MYSQL_SERVICE_HOST"));
+define( 'DB_PORT', getenv("MYSQL_SERVICE_HOST") );
+define( 'DB_NAME', getenv("MYSQL_DATABASE"));
+define( 'DB_USER', getenv("MYSQL_USER") );
+define( 'DB_PASSWORD', getenv("MYSQL_ROOT_PASSWORD") );
 define( 'DB_TABLE', 'PERSONNE' );
 
 //Connexion
